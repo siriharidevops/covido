b0e2be2f8cd93fac495bdd77ff153df3d083ab79
Hareesh <nhareesh1618@gmailc.om>
2023-04-05 21:16:21 +0530

    2nd commit

--- src/main/java/com/urotaxi/repositories/ContactUsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.urotaxi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.urotaxi.entities.ContactUs;
-
-public interface ContactUsRepository extends JpaRepository<ContactUs, Integer> {
-
-}
--- src/main/java/com/urotaxi/repositories/RideRepository.java
+++
@@ -1,9 +0,0 @@
-package com.urotaxi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.urotaxi.entities.Ride;
-
-public interface RideRepository extends JpaRepository<Ride, Integer> {
-
-}
